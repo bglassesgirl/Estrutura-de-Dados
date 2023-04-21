581f29a5dd84a637acb668d65407ca5a69c6e115
Luana Braga <108040997+bglassesgirl@users.noreply.github.com>
2023-04-21 11:23:09 -0300

    Delete Exercicios de Revisão Linguagens 1 directory

--- Exercicios de Revisão Linguagens 1/src/exercicio1/Servico.java
+++
@@ -1,9 +0,0 @@
-package exercicio1;
-
-public class Servico {
-
-	public static int multiplicacao(int numero, int numero2){
-	  return (numero*numero2);     
-	
-	}
-}
--- Exercicios de Revisão Linguagens 1/src/exercicio3/Principal.java
+++
@@ -1,7 +0,0 @@
-package exercicio3;
-
-public class Principal {
-  public static void main(String[] args) {
-	   Servico.fibonacci();
- }
-}
