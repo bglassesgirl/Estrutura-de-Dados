c74f45299b82af4c43d91585c0dedb02057fe486
Luana Braga <108040997+bglassesgirl@users.noreply.github.com>
2023-05-22 17:51:53 -0300

    Update MainJogoDaVelha.java

--- ExerArraysMatriz/src/principal/MainJogoDaVelha.java
+++ ExerArraysMatriz/src/principal/MainJogoDaVelha.java
@@ -7,47 +7,45 @@ import servicos.JogoDaVelha;
 public class MainJogoDaVelha {
 	  public static void main(String[] args) {
 	        
-	    	char[][] tabuleiro = new char[3][3];
-	        char jogadorAtual = 'X';
-	        boolean jogoAcabou = false;
-	        
-	        Scanner scan = new Scanner(System.in);
-	        JogoDaVelha s = new JogoDaVelha();
-	        
-	        s.inicializarTabuleiro(tabuleiro);
-
-	        while (!jogoAcabou) {
-	            s.desenharTabuleiro(tabuleiro);
-
-	            System.out.print("Jogador " + jogadorAtual + ", escolha a linha (1-3): ");
-	            int linha = scan.nextInt() - 1;
-
-	            System.out.print("Jogador " + jogadorAtual + ", escolha a coluna (1-3): ");
-	            int coluna = scan.nextInt() - 1;
-	            
-	            if (tabuleiro[linha][coluna] != ' ') {
-	                System.out.println("Posição ocupada, escolha outra posição.");
-	                continue;
-	            }
-
-	            tabuleiro[linha][coluna] = jogadorAtual;
-
-	            if (s.verificarVitoria(tabuleiro, jogadorAtual)) {
-	            	s.desenharTabuleiro(tabuleiro);
-	            	System.out.println("Jogador " + jogadorAtual + " venceu!");
-	                jogoAcabou = true;
-	            } else if (s.verificarEmpate(tabuleiro)) {
-	                System.out.println("Empate!");
-	                jogoAcabou = true;
-	            }
-
-	            if (jogadorAtual == 'X') {
-	            	  jogadorAtual = 'O';
-	            	} else {
-	            	  jogadorAtual = 'X';
-	            	}
-	        }
-	        
-	        scan.close();
-	    }
+Scanner in = new Scanner(System.in);	  
+	   
+	   char[][] tab = new char[3][3];
+	   char jogador = 'X';
+	   boolean gameOver = false;
+	   JogoDaVelha jv = new JogoDaVelha();
+	   
+	   
+	   for (int l = 0; l < tab.length; l++) {
+		  for (int c = 0; c < tab[l].length; c++) {
+			tab[l][c] = ' ';	
+		}
+	  }
+		
+	  
+	   while(!gameOver) {
+		   jv.printTab(tab);
+		   System.out.println("\nJogador " + jogador + " , jogue entre 1-3 para linha,\n"
+		   		+ "e de 1-3 para coluna, conforme a jogada desejada: ");
+		   int linha = in.nextInt() - 1;
+		   int coluna = in.nextInt() -1 ;
+		   
+		   if (tab[linha][coluna] == ' ') {
+			tab[linha][coluna] = jogador;
+			gameOver = jv.hasWon(tab, jogador);
+			
+			if(gameOver) {
+				 System.out.println("Jogador " + jogador + "  , ganhou!! ");
+			}else {
+				jogador = (jogador == 'X')? 'O' : 'X';
+			}
+		}else {
+			System.out.println("Movimento inválido. Tente de novo.");
+		}
+	   }
+		   
+		  jv.printTab(tab);
+	   
+	   
+   }
+	    
 }
