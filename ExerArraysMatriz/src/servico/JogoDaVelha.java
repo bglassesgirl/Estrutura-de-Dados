882c75598360255964752ae5eff4eefd82738f59
Luana Braga <108040997+bglassesgirl@users.noreply.github.com>
2023-05-22 17:50:41 -0300

    Update JogoDaVelha.java

--- ExerArraysMatriz/src/servico/JogoDaVelha.java
+++ ExerArraysMatriz/src/servico/JogoDaVelha.java
@@ -1,53 +1,41 @@
 package servicos;
 
 public class JogoDaVelha {
-	public void inicializarTabuleiro(char[][] tabuleiro) {
-        for (int i = 0; i < tabuleiro.length; i++) {
-            for (int j = 0; j < tabuleiro[i].length; j++) {
-                tabuleiro[i][j] = ' ';
-            }
-        }
-    }
+	
+	public void printTab(char[][] tab) {
+	  System.out.println("--------------");
+		
+	   for (int l = 0; l < tab.length; l++) {
+		   System.out.print("| ");
+			  for (int c = 0; c < tab[l].length; c++) {
+		       System.out.print(tab[l][c] + " | ");
+			}
+			  System.out.println();
+			  System.out.println("--------------");
+			
+		  }
+	}
+	
 
-    public void desenharTabuleiro(char[][] tabuleiro) {
-        System.out.println("-------------");
-
-        for (int i = 0; i < tabuleiro.length; i++) {
-            System.out.print("| ");
-            for (int j = 0; j < tabuleiro[i].length; j++) {
-                System.out.print(tabuleiro[i][j] + " | ");
-            }
-            System.out.println();
-            System.out.println("-------------");
-        }
-    }
-
-    public boolean verificarVitoria(char[][] tabuleiro, char jogador) {
-        for (int i = 0; i < tabuleiro.length; i++) {
-            if (tabuleiro[i][0] == jogador && tabuleiro[i][1] == jogador && tabuleiro[i][2] == jogador) {
-                return true;
-            }
-            if (tabuleiro[0][i] == jogador && tabuleiro[1][i] == jogador && tabuleiro[2][i] == jogador) {
-                return true;
-            }
-        }
-        if (tabuleiro[0][0] == jogador && tabuleiro[1][1] == jogador && tabuleiro[2][2] == jogador) {
-            return true;
-        }
-        if (tabuleiro[0][2] == jogador && tabuleiro[1][1] == jogador && tabuleiro[2][0] == jogador) {
-            return true;
-        }
-        return false;
-    }
-    
-    public boolean verificarEmpate(char[][] tabuleiro) {
-        for (int linha = 0; linha < tabuleiro.length; linha++) {
-            for (int coluna = 0; coluna < tabuleiro[linha].length; coluna++) {
-                if (tabuleiro[linha][coluna] == ' ') {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+	public  boolean hasWon(char[][] tab, char jogador) {
+		//analisando as linhas
+		for (int l = 0;l < tab.length; l++) {
+			if (tab[l][0] == jogador && tab[l][1] == jogador && tab[l][2] == jogador) {
+				return true;
+			}
+		}
+		//analisando as colunas
+		for (int c = 0; c < tab[0].length; c++) {
+			if (tab[0][c] == jogador && tab[1][c] == jogador && tab[2][c] == jogador) {
+				return true;
+			}
+		}
+		if (tab[0][0] == jogador && tab[1][1] == jogador && tab[2][2] == jogador) {
+			return true;
+		}
+		if (tab[0][2] == jogador && tab[1][1] == jogador && tab[2][0] == jogador) {
+			return true;
+		}
+		return false;
+	}
 }
