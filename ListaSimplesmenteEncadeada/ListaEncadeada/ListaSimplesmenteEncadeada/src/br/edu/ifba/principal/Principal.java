c9080fdbab42ce7be860b651f19834f408f23c52
Luana Braga <108040997+bglassesgirl@users.noreply.github.com>
2023-05-24 12:13:45 -0300

    first 0524

---
+++ ListaSimplesmenteEncadeada/ListaEncadeada/ListaSimplesmenteEncadeada/src/br/edu/ifba/principal/Principal.java
@@ -0,0 +1,19 @@
+package br.edu.ifba.principal;
+
+import br.edu.ifba.lista.Lista;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Lista lista = new Lista();
+		lista.adicionarNoComeco("Luana");
+		lista.adicionarNoComeco("Beatriz");
+		lista.adicionarNoComeco("Julia");
+		lista.adicionarNoComeco("Murilo");
+		lista.adicionarNoComeco("Igor");
+		
+		lista.listar();
+	}
+	
+}
