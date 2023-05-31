77543f22778eacaaeb325e66f819025aa8c84f09
Luana Braga <108040997+bglassesgirl@users.noreply.github.com>
2023-05-31 12:09:05 -0300

    Update Principal.java

--- ListaSimplesmenteEncadeada/ListaEncadeada/ListaSimplesmenteEncadeada/src/br/edu/ifba/principal/Principal.java
+++ ListaSimplesmenteEncadeada/ListaEncadeada/ListaSimplesmenteEncadeada/src/br/edu/ifba/principal/Principal.java
@@ -13,19 +13,47 @@ public class Principal {
 		lista.adicionarNoComeco("Murilo");
 		lista.adicionarNoComeco("Igor");
 		
+		System.out.println("Listar elementos adicionados: ");
 		lista.listar();
 		System.out.println();
 		
 		lista.adicionarNoFinal("Maria Luiza");
 		lista.adicionarNoFinal("Jennie");
 		
-		
+		System.out.println("Listar elementos adicionados no final: ");
 		lista.listar();
 		System.out.println();
+		
 		
 		lista.adicionarPorPosicao(0, "Lorena");
 		lista.adicionarPorPosicao(1, "Giovana");
 		
+		
+		System.out.println("Listar elementos adicionados por posição [posicao 0,1]: ");
+		lista.listar();
+		System.out.println();
+		
+	
+		lista.removeDoComeco();
+		System.out.println("Listar elementos removidos do começo: ");
+		lista.listar();
+		System.out.println();
+		
+		
+		lista.removeFinal();
+		System.out.println("Listar elementos removidos no final: ");
+		lista.listar();
+		System.out.println();
+		
+		
+		lista.adicionarNoFinal("Alisson");
+		System.out.println("Listar último elementos adicionados no final: ");
+		lista.listar();
+		System.out.println();
+		
+		
+		lista.removePosicao(4);
+		System.out.println("Listar elementos removidos no meio[posicao 4]: ");
 		lista.listar();
 		System.out.println();
 		
