53b9acf960f09b85406be5a8835ef066d92a21e9
Luana Braga <108040997+bglassesgirl@users.noreply.github.com>
2023-09-20 12:13:37 -0300

    0920

---
+++ recursividade/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Recursividade recursao = new Recursividade();
+
+        System.out.println(recursao.soma(10));
+    }
+}
